7263bc834a4c3551d69de86635a0c333c3047ba9
zzs <296099296@qq.com>
2018-09-30 00:25:06 +0800

    add btn title

--- src/views/project/detail.vue
+++ src/views/project/detail.vue
@@ -2,7 +2,7 @@
   <div class="detail">
     <el-card class="box-card">
       <div slot="header">
-        <el-button size="mini" style="float: left;top: -5px;position: relative;">
+        <el-button size="mini" style="float: left;top: -5px;position: relative;" title="返回">
           <i class="mdi mdi-arrow-left-bold"/>
         </el-button>
         项目列表
