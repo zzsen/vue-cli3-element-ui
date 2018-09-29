54bd42fdef7c02a831be71ec919d12cd0a01d66b
zzs <296099296@qq.com>
2018-09-30 00:21:11 +0800

    add back btn

--- src/views/project/detail.vue
+++ src/views/project/detail.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="detail">
     <el-card class="box-card">
-      <div slot="header" class="clearfix">
+      <div slot="header">
+        <el-button size="mini" style="float: left;top: -5px;position: relative;">
+          <i class="mdi mdi-arrow-left-bold"/>
+        </el-button>
         项目列表
       </div>
     </el-card>
