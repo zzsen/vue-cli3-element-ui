b643257f429632c7e016b3e66e037f48ad312305
zzs <296099296@qq.com>
2019-01-31 00:53:31 -0800

    edit bgImage

--- src/App.vue
+++ src/App.vue
@@ -89,6 +89,7 @@ export default {
 </script>
 
 <style lang='scss'>
+@import "assets/common.scss";
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
